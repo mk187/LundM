95944e14f5ce76643bd40ce3d1d66b906de77f58
Jan Klostermann <j.klostermann@posteo.de>
2017-09-06 19:48:54 +0200

    Initial commit.

---
+++ backend/src/main/java/de/gnoll/lum/LumApplication.java
@@ -0,0 +1,12 @@
+package de.gnoll.lum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LumApplication.class, args);
+	}
+}
