76a02810fa7784b8f1873ee5fa8d129f5f58706a
Jan Klostermann <j.klostermann@posteo.de>
2017-09-09 14:49:01 +0200

    Added routing and login component.

--- frontend/src/main/frontend/src/app/app.module.ts
+++ frontend/src/main/frontend/src/app/app.module.ts
@@ -1,13 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import {LoginComponent} from "./components/login.component";
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes
+    ),
     BrowserModule
   ],
   providers: [],
